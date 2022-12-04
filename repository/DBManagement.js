2c29b2a6f7e44cd57d0f61f44d9d3490f8dc42d4
gamjalee <jfamily0309@gmail.com>
2022-12-04 12:40:24 +0900

    C:/Program Files/Git/stock/updateProductNum 잘 됨

--- repository/DBManagement.js
+++ repository/DBManagement.js
@@ -2,16 +2,16 @@ import mysql from "mysql";
 import { resolve } from "path";
 
 const connect = () => {
-	const config = {
-		host: "localhost",
-		port: "3306",
-		user: "root",
-		password: "0405",
-		database: "ku_pang",
-	};
-	const conn = mysql.createConnection(config);
-	conn.connect();
-	return conn;
+  const config = {
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "0405",
+    database: "ku_pang",
+  };
+  const conn = mysql.createConnection(config);
+  conn.connect();
+  return conn;
 };
 
 /**
@@ -21,28 +21,28 @@ const connect = () => {
  * @param {list} values [12345, "물품"]
  */
 const insert = (table, fields, values) => {
-	let conn = connect();
-	let fieldsdStr = "";
-	let valuesStr = "";
-	for (const field of fields) {
-		fieldsdStr += field + ",";
-	}
-	fieldsdStr = fieldsdStr.substring(0, fieldsdStr.length - 1);
-	for (const value of values) {
-		if (typeof value === "string") valuesStr += "'" + value + "'" + ",";
-		else valuesStr += value + ",";
-	}
-	valuesStr = valuesStr.substring(0, valuesStr.length - 1);
-	let sql = `INSERT INTO ${table} (${fieldsdStr}) VALUES (${valuesStr})`;
-	conn.query(sql, (err, result) => {
-		if (err) {
-			console.log(err + ":: INSERT FAIL!");
-			throw new Error("QUERY FAIL");
-		}
-		console.log("INSERT SUCCESS!");
-		conn.end();
-		return result;
-	});
+  let conn = connect();
+  let fieldsdStr = "";
+  let valuesStr = "";
+  for (const field of fields) {
+    fieldsdStr += field + ",";
+  }
+  fieldsdStr = fieldsdStr.substring(0, fieldsdStr.length - 1);
+  for (const value of values) {
+    if (typeof value === "string") valuesStr += "'" + value + "'" + ",";
+    else valuesStr += value + ",";
+  }
+  valuesStr = valuesStr.substring(0, valuesStr.length - 1);
+  let sql = `INSERT INTO ${table} (${fieldsdStr}) VALUES (${valuesStr})`;
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.log(err + ":: INSERT FAIL!");
+      throw new Error("QUERY FAIL");
+    }
+    console.log("INSERT SUCCESS!");
+    conn.end();
+    return result;
+  });
 };
 
 /**
@@ -52,28 +52,28 @@ const insert = (table, fields, values) => {
  * @param {string, undefined} whereOptions "stockId=7"
  */
 const select = async (fields, table, whereOptions) => {
-	let conn = connect();
-	let fieldsdStr = "";
-	for (const field of fields) {
-		fieldsdStr += field + ",";
-	}
-	fieldsdStr = fieldsdStr.substring(0, fieldsdStr.length - 1);
+  let conn = connect();
+  let fieldsdStr = "";
+  for (const field of fields) {
+    fieldsdStr += field + ",";
+  }
+  fieldsdStr = fieldsdStr.substring(0, fieldsdStr.length - 1);
 
-	let sql = `SELECT ${fieldsdStr} FROM ${table}`;
-	if (whereOptions != undefined && whereOptions.length != 0) {
-		sql += " WHERE " + whereOptions;
-	}
-	console.log(sql);
-	return new Promise((res, rej) => {
-		conn.query(sql, (err, result) => {
-			if (err) {
-				console.log(err);
-				throw new Error("QUERY FAIL");
-			}
-			res(result);
-			console.log(result);
-		});
-	});
+  let sql = `SELECT ${fieldsdStr} FROM ${table}`;
+  if (whereOptions != undefined && whereOptions.length != 0) {
+    sql += " WHERE " + whereOptions;
+  }
+  console.log(sql);
+  return new Promise((res, rej) => {
+    conn.query(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+        throw new Error("QUERY FAIL");
+      }
+      res(result);
+      console.log(result);
+    });
+  });
 };
 
 /**
@@ -83,20 +83,20 @@ const select = async (fields, table, whereOptions) => {
  * @param {string} whereOptions "stockId=7"
  */
 const update = (table, updateOptions, whereOptions) => {
-	let conn = connect();
-	let sql = `UPDATE ${table} SET ${updateOptions}`;
-	if (whereOptions != undefined && whereOptions.length != 0) {
-		sql += " WHERE " + whereOptions;
-	}
-	console.log(sql);
-	conn.query(sql, (err, result) => {
-		if (err) {
-			console.log(err + ":: UPDATE FAIL!");
-			throw new Error("QUERY FAIL");
-		}
-		console.log("UPDATE SUCCESS!");
-		conn.end();
-	});
+  let conn = connect();
+  let sql = `UPDATE ${table} SET ${updateOptions}`;
+  if (whereOptions != undefined && whereOptions.length != 0) {
+    sql += " WHERE " + whereOptions;
+  }
+  console.log(sql);
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.log(err + ":: UPDATE FAIL!");
+      throw new Error("QUERY FAIL");
+    }
+    console.log("UPDATE SUCCESS!");
+    conn.end();
+  });
 };
 
 /**
@@ -105,19 +105,19 @@ const update = (table, updateOptions, whereOptions) => {
  * @param {string} whereOptions "stockNum=3"
  */
 const del = (table, whereOptions) => {
-	const conn = connect();
-	let sql = `DELETE FROM ${table}`;
-	if (whereOptions != undefined && whereOptions.length != 0) {
-		sql += " WHERE " + whereOptions;
-	}
-	conn.query(sql, (err, result) => {
-		if (err) {
-			console.log(err + ":: DELETE FAIL!");
-			throw new Error("QUERY FAIL");
-		}
-		console.log("DELETE SUCCESS!");
-		conn.end();
-	});
+  const conn = connect();
+  let sql = `DELETE FROM ${table}`;
+  if (whereOptions != undefined && whereOptions.length != 0) {
+    sql += " WHERE " + whereOptions;
+  }
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.log(err + ":: DELETE FAIL!");
+      throw new Error("QUERY FAIL");
+    }
+    console.log("DELETE SUCCESS!");
+    conn.end();
+  });
 };
 
 export { insert, select, update, del };
